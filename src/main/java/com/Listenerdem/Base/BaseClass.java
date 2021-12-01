f57df01e43f09c21dae21ac4f6a0a045fbed0784
@sumit-n31! <srn3112@gmail.com>
2021-12-01 10:54:49 +0530

    adding file

--- src/main/java/com/Listenerdem/Base/BaseClass.java
+++ src/main/java/com/Listenerdem/Base/BaseClass.java
@@ -2,6 +2,7 @@ package com.Listenerdem.Base;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.Listenerdem.UIKeywords.UIKeywords;
@@ -15,8 +16,14 @@ public class BaseClass {
 		ui.openBrowser("");
 
 	}
+	@AfterMethod
 	public void tearDown() {
 		ui.closeBrowser();
+	}
+	
+	public void m1() {
+		// TODO Auto-generated method stub
+
 	}
 
 }
